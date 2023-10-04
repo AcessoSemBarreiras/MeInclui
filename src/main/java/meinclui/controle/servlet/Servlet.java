c8b136e22b4ff76503b907a7a035451374aac719
Christian <christian.ads25.10.05@gmail.com>
2023-10-04 11:02:41 -0300

    Iniciado roteamento no Servlet

--- src/main/java/meinclui/controle/servlet/Servlet.java
+++ src/main/java/meinclui/controle/servlet/Servlet.java
@@ -1,5 +1,75 @@
 package meinclui.controle.servlet;
 
-public class Servlet {
+import java.io.IOException;
+import java.sql.SQLException;
 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import meinclui.modelo.dao.UsuarioTemConquista.UsuarioTemConquistaDAO;
+import meinclui.modelo.dao.UsuarioTemConquista.UsuarioTemConquistaDAOImpl;
+import meinclui.modelo.dao.avaliacao.AvaliacaoDAO;
+import meinclui.modelo.dao.avaliacao.AvaliacaoDAOImpl;
+import meinclui.modelo.dao.categoria.CategoriaDAO;
+import meinclui.modelo.dao.categoria.CategoriaDAOImpl;
+import meinclui.modelo.dao.comentario.ComentarioDAO;
+import meinclui.modelo.dao.comentario.ComentarioDAOImpl;
+import meinclui.modelo.dao.conquista.ConquistaDAO;
+import meinclui.modelo.dao.conquista.ConquistaDAOImpl;
+import meinclui.modelo.dao.endereco.EnderecoDAO;
+import meinclui.modelo.dao.endereco.EnderecoDAOImpl;
+import meinclui.modelo.dao.estabelecimento.EstabelecimentoDAO;
+import meinclui.modelo.dao.estabelecimento.EstabelecimentoDAOImpl;
+import meinclui.modelo.dao.usuario.UsuarioDAO;
+import meinclui.modelo.dao.usuario.UsuarioDAOImpl;
+
+@WebServlet("/")
+public class Servlet extends HttpServlet {
+	
+	private static final long serialVersionUID = 1L;
+	private AvaliacaoDAO avaliacaoDAO;
+	private CategoriaDAO categoriaDAO;
+	private ComentarioDAO comentarioDAO;
+	private ConquistaDAO conquistaDAO;
+	private EnderecoDAO enderecoDAO;
+	private EstabelecimentoDAO estabelecimentoDAO;
+	private UsuarioDAO usuarioDAO;
+	private UsuarioTemConquistaDAO usuarioTemConquistaDAO;
+	
+	public void init() {
+		avaliacaoDAO = new AvaliacaoDAOImpl();
+		categoriaDAO = new CategoriaDAOImpl();
+		comentarioDAO = new ComentarioDAOImpl();
+		conquistaDAO = new ConquistaDAOImpl();
+		enderecoDAO = new EnderecoDAOImpl();
+		estabelecimentoDAO = new EstabelecimentoDAOImpl();
+		usuarioDAO = new UsuarioDAOImpl();
+		usuarioTemConquistaDAO = new UsuarioTemConquistaDAOImpl();
+	}
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doGet(request, response);
+	}
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+		String action = request.getServletPath();
+		
+		try {
+			
+			switch (action) {
+			case "/login":
+				mostrarFormularioLogin(request, response);
+				break;
+			}} catch (SQLException ex) {
+				throw new ServletException(ex);
+			}
+		}
+	
 }
+
