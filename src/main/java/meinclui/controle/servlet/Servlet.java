b3908114bc8119ff65732d36244080ba8899fb90
Christian <christian.ads25.10.05@gmail.com>
2023-10-06 09:49:53 -0300

    Feito o roteamento

--- src/main/java/meinclui/controle/servlet/Servlet.java
+++ src/main/java/meinclui/controle/servlet/Servlet.java
@@ -63,28 +63,110 @@ public class Servlet extends HttpServlet {
 		try {
 			
 			switch (action) {
+			case "/tela-inicial":
+				mostrarTelaInicial(request, response);
+				break;
+				
 			case "/login-usuario":
 				mostrarFormularioLogin(request, response);
 				break;
 			case "/cadastro-usuario":
 				mostrarFormularioCadastroUsuario(request, response);
 				break;
-			case "/ranque-usuario":
-				mostrarRanque(request, response);
+			case "/inserir-usuario":
+				inserirUsuario(request, response);
+				break;
+			case "/deletar-usuario":
+				deletarUsuario(request, response);
+				break;
+			case "/atualizar-usuario":
+				atualizarUsuario(request, response);
 				break;
 			case "/perfil-usuario":
 				mostrarPerfilUsuario(request, response);
 				break;
+			case "/ranque-usuario":
+				mostrarRanque(request, response);
+				break;
+			case "/editar-perfil-usuario":
+				mostrarFormularioEditarUsuario(request, response);
+				break;
+				
 			case "/cadastro-estabelecimento":
 				mostrarFormularioCadastroEstabelecimento(request, response);
+				break;
+			case "/inserir-estabelecimento":
+				inserirEstabelecimento(request, response);
+				break;
+			case "/deletar-estabelecimento":
+				deletarEstabelecimento(request, response);
+				break;
+			case "/atualizar-estabelecimento":
+				atualizarEstabelecimento(request, response);
+				break;
+			case "/perfil-estabelecimento":
+				mostrarPerfilEstabelecimento(request, response);
 				break;
 			case "/avaliacao-estabelecimento":
 				mostrarFormularioAvaliacaoEstabelecimento(request, response);
 				break;
-			case "/cadastrar-comentario";
-				mostrarFormularioComentario(request, response);
+			case "/encontrar-estabelecimentos":
+				mostrarTelaPesquisaEstabelecimento(request, response);
+				break;
+			case "/editar-perfil-estabelecimento":
+				mostrarFormularioEditarEstabelecimento(request, response);
 				break;
 				
+			case "/cadastrar-comentario":
+				mostrarFormularioCadastroComentario(request, response);
+				break;
+			case "/inserir-comentario":
+				inserirComentario(request, response);
+				break;
+			case "/deletar-comentario":
+				deletarComentario(request, response);
+				break;
+			case "/atualizar-comentario":
+				atualizarComentario(request, response);
+				break;
+			case "/recuperar-comentario":
+				recuperarComentario(request, response);
+				break;
+				
+			case "/cadastrar-avaliacao":
+				mostrarFormularioCadastroAvaliacao(request, response);
+				break;
+			case "/inserir-avaliacao":
+				inserirAvaliacao(request, response);
+				break;
+			case "/deletar-avaliacao":
+				deletarAvaliacao(request, response);
+				break;
+			case "/atualizar-avaliacao":
+				atualizarAvaliacao(request, response);
+				break;
+			case "/recuperar-avaliacao":
+				recuperarAvaliacao(request, response);
+				break;
+				
+			case "/cadastrar-endereco":
+				mostrarFormularioCadastroEndereco(request, response);
+				break;
+			case "/inserir-endereco":
+				inserirEndereco(request, response);
+				break;
+			case "/deletar-endereco":
+				deletarEndereco(request, response);
+				break;
+			case "/atualizar-endereco":
+				atualizarEndereco(request, response);
+				break;
+			case "/recuperar-endereco":
+				recuperarEndereco(request, response);
+				break;
+			
+			
+			
 			}} catch (SQLException ex) {
 				throw new ServletException(ex);
 			}
